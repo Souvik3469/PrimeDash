b16c20491fda609caedd070b1ab8791bccd39d08
Souvik3469 <souviksen3469@gmail.com>
2024-11-26 14:43:39 +0530

    Fix: integration table css

--- src/components/Comp3.tsx
+++ src/components/Comp3.tsx
@@ -23,79 +23,70 @@ ChartJS.register(
   LinearScale
 );
 
-const IntegrationRow: React.FC<{
+const IntegrationTableRow: React.FC<{
   icon: string;
   name: string;
   type: string;
   rate: number;
   profit: string;
   hasBorder?: boolean;
-  hasPadding?: boolean;
-}> = ({
-  icon,
-  name,
-  type,
-  rate,
-  profit,
-  hasBorder = true,
-  hasPadding = true,
-}) => {
+}> = ({ icon, name, type, rate, profit, hasBorder = true }) => {
   return (
-    <div
-      className={`grid grid-cols-4 text-[#00000099] dark:text-[#FFFFFF99] items-center px-1  ${
+    <tr
+      className={`${
         hasBorder
-          ? "border-b-2 border-[#00000029] dark:border-[#FFFFFF29] mb-6"
+          ? " border-b-2 border-[#00000029] dark:border-[#FFFFFF29] "
           : ""
       }
-          ${hasPadding ? "pb-6" : ""}
-         
-      `}
+   
+    `}
     >
-      <div className="flex items-center">
-        <input
-          type="checkbox"
-          className="mr-3 align-middle"
-          style={{
-            width: "15px",
-            height: "15px",
-          }}
-        />
+      <td className=" py-6 px-4">
         <div className="flex items-center">
-          <img
-            src={icon}
-            className="h-6 w-6 mr-2 border-2 border-[#00000029] dark:border-[#FFFFFF29]"
+          <input
+            type="checkbox"
+            className="mr-3 align-middle"
+            style={{
+              width: "15px",
+              height: "15px",
+            }}
           />
-          <div className="text-sm">{name}</div>
-        </div>
-      </div>
-      <div className="text-sm text-center">{type}</div>
-      <div className="flex flex-col items-center">
-        <div className="flex w-full justify-center ml-12">
-          <div className="flex ml-12">
-            <div className="flex w-28 justify-center items-center">
-              <div
-                className="h-1.5"
-                style={{
-                  width: `${rate}%`,
-                  backgroundColor: "#696FFB",
-                  borderRadius: "4px",
-                }}
-              ></div>
-              <div
-                className="h-1.5"
-                style={{
-                  width: `${100 - rate}%`,
-                  backgroundColor: "#696FFB33",
-                  borderRadius: "4px",
-                }}
-              ></div>
-            </div>
-            <div className="text-xs ml-2">{rate}%</div>
+          <div className="flex items-center">
+            <img
+              src={icon}
+              className="h-6 w-6 mr-2 border-2 border-[#00000029] dark:border-[#FFFFFF29]"
+            />
+            <div className="text-sm">{name}</div>
           </div>
         </div>
-      </div>
-      <div className="text-sm text-right">{profit}</div>
-    </div>
+      </td>
+      <td className=" px-4 py-6 text-sm">{type}</td>
+
+      <td className="px-4 py-6 ">
+        <div className="w-40 h-2  rounded flex  items-center">
+          {/* <div className={`w-[100%] h-2 bg-green-500 rounded`}></div> */}
+          <div
+            style={{
+              width: `${rate}%`,
+              backgroundColor: "#696FFB",
+              borderRadius: "4px",
+            }}
+            className="h-1.5"
+          ></div>
+          <div
+            style={{
+              width: `${100 - rate}%`,
+              backgroundColor: "#696FFB33",
+              borderRadius: "4px",
+            }}
+            className="h-1.5"
+          ></div>
+          <span className="text-xs ml-4">{rate}%</span>
+        </div>
+      </td>
+
+      <td className=" text-sm px-4 py-6">{profit}</td>
+    </tr>
   );
 };
 
@@ -141,51 +132,56 @@ const Comp3: React.FC = () => {
 
       {/* Sales Integration */}
       <div className="bg-white dark:bg-[#1F214A] dark:border-[#1F214A] dark:text-white border-2 border-gray-200 p-4 rounded-lg col-span-12 md:col-span-7 lg:col-span-8">
-        <h2 className="text-xl font-bold mb-4">Sales Integration</h2>
-        <div>
-          {/* Header Row */}
-          <div className="grid grid-cols-4 text-[#00000099] dark:text-[#FFFFFF99] bg-[#696FFB14] dark:bg-[#FFFFFF14] p-2 rounded-lg px-1 pr-6 mb-6">
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                className="mr-3 align-middle"
-                style={{
-                  width: "15px",
-                  height: "15px",
-                }}
-              />
-              <div className="text-sm">Application</div>
-            </div>
-            <div className="text-sm text-center">Type</div>
-            <div className="text-sm text-center">Rate</div>
-            <div className="text-sm text-right">Profit</div>
-          </div>
+        <h2 className="text-xl font-bold mb-6">List of Integration</h2>
+        <table className="min-w-full table-auto  text-[#00000099] dark:text-[#FFFFFF99] ">
+          <thead className="">
+            <tr className="bg-[#696FFB14] dark:bg-[#FFFFFF14]">
+              <th className="px-4 py-2 text-left text-sm first:rounded-l-lg ">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="mr-3 align-middle"
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                    }}
+                  />
+                  <span>Application</span>
+                </div>
+              </th>
+              <th className="px-4 py-2 text-left text-sm ">Type</th>
+              <th className="px-4 py-2 text-left text-sm ">Rate</th>
+              <th className="px-4 py-2 text-left text-sm  last:rounded-r-lg">
+                Profit
+              </th>
+            </tr>
+          </thead>
 
-          {/* Integration Rows */}
-          <IntegrationRow
-            icon={Stripe}
-            name="Stripe"
-            type="Finance"
-            rate={33}
-            profit="$10,998.28"
-          />
-          <IntegrationRow
-            icon={Zapier}
-            name="Zapier"
-            type="CRM"
-            rate={27}
-            profit="$8,998.59"
-          />
-          <IntegrationRow
-            icon={Shopify}
-            name="Shopify"
-            type="Marketplace"
-            rate={40}
-            profit="$13,331.24"
-            hasBorder={false}
-            hasPadding={false}
-          />
-        </div>
+          <tbody>
+            <IntegrationTableRow
+              icon={Stripe}
+              name="Stripe"
+              type="Finance"
+              rate={33}
+              profit="$10,998.28"
+            />
+            <IntegrationTableRow
+              icon={Zapier}
+              name="Zapier"
+              type="CRM"
+              rate={27}
+              profit="$8,998.59"
+            />
+            <IntegrationTableRow
+              icon={Shopify}
+              name="Shopify"
+              type="Marketplace"
+              rate={40}
+              profit="$13,331.24"
+              hasBorder={false}
+            />
+          </tbody>
+        </table>
       </div>
     </div>
   );
